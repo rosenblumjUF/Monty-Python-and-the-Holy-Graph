9f7c36c5031dd030d74539fbe0ff8952dbc508e5
maddiebrewer <mbrewer1@ufl.edu>
2021-12-03 16:34:30 -0500

    cmake-build-debug/names.csv

---
+++ cmake-build-debug/CMakeFiles/Monty_Python_and_the_Holy_Graph.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Monty_Python_and_the_Holy_Graph.dir/Graph.cpp.o"
+  "CMakeFiles/Monty_Python_and_the_Holy_Graph.dir/main.cpp.o"
+  "Monty_Python_and_the_Holy_Graph"
+  "Monty_Python_and_the_Holy_Graph.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Monty_Python_and_the_Holy_Graph.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
