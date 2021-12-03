845ff9bb4d7dc7b5520be64058a45b73ea41a197
mwielatz <mwielatz@ufl.edu>
2021-12-03 15:01:01 -0500

    some graph functions

--- Graph.h
+++ Graph.h
@@ -1,7 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+using std::string;
+using std::pair;
+using std::vector;
+using std::map;
+using std::cout;
+
+
+
 #pragma once
 
-class Graph {
 
+class Graph {
+    private:
+    //Graph as an adjacency list - data member, e.g. Vector, Set, Vector of Vectors, Maps, Lists, etc.
+    map<string, vector<pair<string, int>>> graph;
+
+    public:
+    void insertEdge(int from, int to, int weight);
+    vector<int> getAdjacent(int vertex);
+    int getIndegree(int vertex);
+    int getOutdegree(int vertex);
 };
 
 
