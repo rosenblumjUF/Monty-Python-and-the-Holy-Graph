4f261c8387d76d3457d1a8c278275f2717d776c0
Rosenblumj <rosenblumj@ufl.edu>
2021-12-03 16:27:07 -0500

    Changed to be a vector of pairs :/

--- Graph.h
+++ Graph.h
@@ -10,10 +10,10 @@ class Graph {
     private:
 
     // Graph as an adjacency list - data member, e.g. Vector, Set, Vector of Vectors, Maps, Lists, etc.
-    // Key is IMDb identifier, value is a pair where:
+    // Key is IMDb identifier, value is a vector of pairs where:
     //      first = IMDb identifier of an adjacent actor
     //      second = a vector of movies (using IMDb identifier of movie) that the adjacent actors are in
-    unordered_map<string, pair<string,vector<string>>> graph;
+    unordered_map<string, vector<pair<string, vector<string>>>> graph;
 
     // Unordered maps used to translate IMDb identifier to name of actor or movie
     //      key = IMDb identifier of actor/movie
