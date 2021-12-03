5d1e5e7878d2a3d8631db0b1eed64a13db7f6589
Rosenblumj <rosenblumj@ufl.edu>
2021-12-03 16:20:47 -0500

    Changed graph to include movies in the map as well. Added maps for actor and movie names.

--- Graph.h
+++ Graph.h
@@ -8,8 +8,18 @@ using namespace std;
 
 class Graph {
     private:
+
     //Graph as an adjacency list - data member, e.g. Vector, Set, Vector of Vectors, Maps, Lists, etc.
-    unordered_map<string, vector<string>> graph;
+    // Key is IMDb identifier, value is a pair where:
+    //      first = IMDb identifier of an adjacent actor
+    //      second = a vector of movies (using IMDb identifier of movie) that the adjacent actors are in
+    unordered_map<string, pair<string,vector<string>>> graph;
+
+    // Unordered maps used to translate IMDb identifier to name of actor or movie
+    //      key = IMDb identifier of actor/movie
+    //      value = name/title of actor/movie
+    unordered_map<string, string> actors;
+    unordered_map<string, string> movies;
 
     public:
     Graph();
